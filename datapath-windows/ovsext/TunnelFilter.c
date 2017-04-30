3e6220eae0b2219f40d00e627afa51301dcca480
Alin Gabriel Serdean <aserdean@cloudbasesolutions.com>
2017-05-01 01:09:57 +0300

    windows: Adds Cloudbase logo
    Signed-off-by: Alin Gabriel Serdean <aserdean@cloudbasesolutions.com>

--- datapath-windows/ovsext/TunnelFilter.c
+++ datapath-windows/ovsext/TunnelFilter.c
@@ -50,14 +50,14 @@
 
 /* The provider name should always match the provider string from the install
  * file. */
-#define OVS_TUNNEL_PROVIDER_NAME        L"Open vSwitch"
+#define OVS_TUNNEL_PROVIDER_NAME        L"Cloudbase Open vSwitch"
 
 /* The provider description should always contain the OVS service description
  * string from the install file. */
-#define OVS_TUNNEL_PROVIDER_DESC        L"Open vSwitch Extension tunnel provider"
+#define OVS_TUNNEL_PROVIDER_DESC        L"Cloudbase Open vSwitch Extension tunnel provider"
 
 /* The session name isn't required but it's useful for diagnostics. */
-#define OVS_TUNNEL_SESSION_NAME         L"OVS tunnel session"
+#define OVS_TUNNEL_SESSION_NAME         L"Cloudbase OVS tunnel session"
 
 /* Maximum number of tunnel threads to be created. */
 #define OVS_TUNFLT_MAX_THREADS          8
